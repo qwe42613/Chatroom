0c08103f733073fbc2d0960131a895f24830d0c3
Sheng <mac@Big99deMacBook-Pro.local>
2024-03-14 17:34:48 +0800

    Server function adjust

--- Server.c
+++ Server.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
   serv_addr.sin_port = htons(port);
 
   if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    error("Building Failed");
+    error("Binding Failed");
   listen(sockfd, 5);
   clilen = sizeof(cli_addr);
 
